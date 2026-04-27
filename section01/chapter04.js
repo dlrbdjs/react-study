10ee0658e06cc91e74d85dc118358bee3791e5f9
Gyueon Lee <gon2yq@naver.com>
2026-04-28 02:24:47 +0900

    chapter04 (변수 규칙)

---
+++ section01/chapter04.js
@@ -0,0 +1,25 @@
+// 1. 변수
+let age;
+// console.log(age);
+
+age = 30;
+// console.log(age);
+
+// 2. 상수
+const birth = "2002.07.29";
+
+// 3. 변수 명명규칙 (네이밍 규칙)
+
+// 3-1. $, _를 제외한 기호를 사용할 수 없다
+let $name;
+
+// 3-2. 숫자로 시작할 수 없다
+// let 2name;
+
+// 3-3. 예약어를 사용할 수 없다
+// let let 
+
+// 4. 변수 명명 가이드
+let a = 1;
+let b = 1;
+let c = a - b;
