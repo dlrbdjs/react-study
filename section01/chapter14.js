6d6751ed317f804361e2ecc7bd7040394f41e1d3
Gyueon Lee <gon2yq@naver.com>
2026-05-05 00:44:17 +0900

    chapter14 (스코프)

---
+++ section01/chapter14.js
@@ -0,0 +1,34 @@
+// 스코프
+// -> 전역(전체 영역) 스코프 / 지역(특정 영역) 스코프
+// -> 전역 스코프 : 전체 영역에서 접근 가능
+// -> 지역 스코프 : 특정 영역에서만 접근 가능
+
+let a = 1; // 전역 스코프
+function funcA() {
+    let b = 2; // 지역 스코프
+    console.log(a);
+
+    function funcB() {}
+}
+
+funcA();
+// console.log(b); // 오류 발생
+
+if (true) {
+    let c = 1; // 지역 스코프
+    function funcC() {
+        console.log("C");
+    }
+}
+// console.log(c); // 오류 발생
+
+for (let i = 0; i < 10; i++) {
+    let d = 1;
+    function funcD() {
+        console.log("D");
+    }
+}
+// console.log(d); // 오류 발생
+// funcB(); // 오류 발생
+funcC(); // 함수 선언식만 실행된다
+funcD(); // 함수 선언식만 실행된다
