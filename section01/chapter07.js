7aba314d0a66b293c6cf37dff0f01216441e6572
Gyueon Lee <gon2yq@naver.com>
2026-05-04 21:55:10 +0900

    chapter07 (연산자)

---
+++ section01/chapter07.js
@@ -0,0 +1,39 @@
+// 1. 대입 연산자
+let var1 = 1;
+
+// 2. 산술 연산자
+let num1 = 3 + 2;
+let num2 = 3 - 2;
+let num3 = 3 * 2;
+let num4 = 3 / 2;
+let num5 = 3 % 2;
+
+// 3. 복합 대입 연산자
+let num7 = 10;
+num7 = num7 + 20;
+num7 += 20;
+
+// 4. 증감 연산자
+let num8 = 10;
+num8++;
+
+// 5. 논리 연산자
+let or = true || false;
+
+let and = true && true
+
+let not = !true;
+
+console.log(or, and, not);
+
+// 6. 비교 연산자
+let comp1 = 1 === 2; // '===' <- 값의 자료형 까지 비교
+let comp2 = 1 !== 2;
+let comp3 = 1 == '1'; // 이건 true
+
+let comp4 = 2 > 1;
+let comp5 = 2 < 1;
+console.log(comp3, comp4);
+
+let comp6 = 2 >= 1;
+let comp7 = 2 <= 1;
